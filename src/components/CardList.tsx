c47ce578b94149cca324566c8b3934da7cab42af
Gabriel Souza <gabrielcsg19@hotmail.com>
2021-08-09 22:04:40 -0300

    Criada a lógica do modal

--- src/components/CardList.tsx
+++ src/components/CardList.tsx
@@ -16,17 +16,23 @@ interface CardsProps {
 }
 
 export function CardList({ cards }: CardsProps): JSX.Element {
-  // TODO MODAL USEDISCLOSURE
+  const { isOpen, onClose, onOpen } = useDisclosure();
+  const [imgUrl, setImageUrl] = useState('');
 
-  // TODO SELECTED IMAGE URL STATE
-
-  // TODO FUNCTION HANDLE VIEW IMAGE
+  function handleSetImageUrl(url: string): void {
+    setImageUrl(url);
+    onOpen();
+  }
 
   return (
     <>
-      {/* TODO CARD GRID */}
+      <SimpleGrid columns={3} spacing="40px">
+        {cards.map(card => (
+          <Card key={card.id} data={card} viewImage={handleSetImageUrl} />
+        ))}
+      </SimpleGrid>
 
-      {/* TODO MODALVIEWIMAGE */}
+      <ModalViewImage isOpen={isOpen} onClose={onClose} imgUrl={imgUrl} />
     </>
   );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -18,31 +18,49 @@ export default function Home(): JSX.Element {
     hasNextPage,
   } = useInfiniteQuery(
     'images',
-    ({ pageParam = 0 }) => {
-      return api
-        .get(`/api/images?after=${pageParam}`)
-        .then(response => response);
+    async ({ pageParam = null }) => {
+      const { data: pageData } = await api.get(`/api/images`, {
+        params: { after: pageParam },
+      });
+      return pageData;
     },
     {
-      getNextPageParam: lastPage => lastPage ?? null,
+      getNextPageParam: lastPage => lastPage.after ?? null,
     }
   );
 
   const formattedData = useMemo(() => {
-    return data?.pages.map(page => page.data.data[0]);
+    return data?.pages.flatMap(page => page.data);
   }, [data]);
 
-  // TODO RENDER LOADING SCREEN
+  if (isLoading) {
+    return (
+      <>
+        <Header />
+        <Loading />
+      </>
+    );
+  }
 
-  // TODO RENDER ERROR SCREEN
+  if (isError) {
+    return (
+      <>
+        <Header />
+        <Error />
+      </>
+    );
+  }
 
   return (
     <>
       <Header />
-
       <Box maxW={1120} px={20} mx="auto" my={20}>
         <CardList cards={formattedData} />
-        {/* TODO RENDER LOAD MORE BUTTON IF DATA HAS NEXT PAGE */}
+        {hasNextPage && (
+          <Button mt="8" onClick={() => fetchNextPage()} role="button">
+            {isFetchingNextPage ? 'Carregando...' : 'Carregar mais'}
+          </Button>
+        )}
       </Box>
     </>
   );
